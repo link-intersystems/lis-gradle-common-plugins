a2777c759f11a14d385618dec3316179d55c94bf
René Link <rene.link@link-intersystems.com>
2024-09-07 16:19:17 +0200

    Removed afterEvaluate from signing configuration.

--- src/main/kotlin/com.link-intersystems.gradle.maven-central-artifact.gradle.kts
+++ src/main/kotlin/com.link-intersystems.gradle.maven-central-artifact.gradle.kts
@@ -3,47 +3,23 @@ plugins {
     signing
 }
 
-afterEvaluate {
-    signing {
-        val signingKey: String? by project
-        val signingKeyExists = signingKey != null
+signing {
+    val signingKey: String? by project
+    val signingKeyExists = signingKey != null
 
-        val signingPassword: String? by project
-        val signingPasswordExists = signingPassword != null
+    val signingPassword: String? by project
+    val signingPasswordExists = signingPassword != null
 
-        logger.debug("signingKey exists = {}, signingPassword {}", signingKeyExists, signingPasswordExists)
+    logger.debug("signingKey exists = {}, signingPassword {}", signingKeyExists, signingPasswordExists)
 
-        val signingEnabled = signingKey != null && signingPassword != null
-        logger.debug("signingEnabled = {}", signingEnabled)
+    val signingEnabled = signingKey != null && signingPassword != null
+    logger.debug("signingEnabled = {}", signingEnabled)
 
-        isRequired = signingEnabled
+    isRequired = signingEnabled
 
-        if (signingEnabled) {
-            useInMemoryPgpKeys(signingKey, signingPassword)
-            sign(publishing.publications)
+    sign(publishing.publications)
 
-            val publishedGAVs = publishing.publications.withType(MavenPublication::class).flatMap {
-                val groupId = it.groupId
-                val artifactId = it.artifactId
-                val artifactVersion = it.version
-
-                val pomGav = "$groupId:$artifactId:pom:$artifactVersion"
-                val allArtifactGAVs = mutableSetOf(pomGav)
-
-                if (it.artifacts.isNotEmpty()) {
-                    val artifactGAVs = it.artifacts.filter { a -> a.extension != null }.map {
-                        val extension = it.extension
-                        "$groupId:$artifactId:$extension:$artifactVersion"
-                    }
-                    allArtifactGAVs.addAll(artifactGAVs)
-                }
-
-                allArtifactGAVs
-            }.joinToString(separator = "\n  - ", prefix = "  - ")
-
-            logger.lifecycle("Signing publications: \n$publishedGAVs")
-        } else {
-            logger.info("Signing disabled. Set the GPG_SIGNING_KEY and GPG_SIGNING_PASSPHRASE environment variable to enable.")
-        }
+    if (signingEnabled) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
     }
 }
