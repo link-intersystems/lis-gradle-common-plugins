93fb115c0d65ecd8a4ec171c56a3aba946962dd9
René Link <rene.link@link-intersystems.com>
2024-10-05 18:27:19 +0200

    Updated lis-gradle-common-plugins version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,9 @@ pluginManagement {
 
 plugins {
     id("com.link-intersystems.gradle.multi-module") version "0.5.4"
-    id("com.link-intersystems.gradle.maven-central-artifact") version "0.0.10" apply false
-    id("com.link-intersystems.gradle.maven-central-project") version "0.0.10" apply false
-    id("com.link-intersystems.gradle.maven-central-java") version "0.0.10" apply false
+    id("com.link-intersystems.gradle.maven-central-artifact") version "0.0.11" apply false
+    id("com.link-intersystems.gradle.maven-central-project") version "0.0.11" apply false
+    id("com.link-intersystems.gradle.maven-central-java") version "0.0.11" apply false
 }
 
 dependencyResolutionManagement {
